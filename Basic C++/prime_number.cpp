75e37999bd3939dbbdf00f6acb2a0c943a36d299
My Demo <dk0778671@gmail.com>
2024-06-05 09:02:42 +0530

    new code added readme.md

---
+++ Basic C++/prime_number.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter your number:";
+    cin>>n;
+    //Appoach 1
+    /*if(n%2==0){
+        cout<<" note prime";
+    }else{
+    cout<<" prime";
+    }*/
+    //Approach 2 
+    bool flag=0;
+    for(int i=2;i<=sqrt(n);i++){
+        if(n%i==0){
+            cout<<"not a prime"<<endl;
+            flag=1;
+            break;
+        }
+    }
+    if(flag==0){
+        cout<<"prime";
+    }
+    return 0;
+}
